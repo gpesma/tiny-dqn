174632db0fb7e87e786c12cd7e1f8804ff71421d
gpesma <gpesmazoglou@gmail.com>
2018-02-20 12:07:43 -0500

    pngs

--- make_graph.py
+++ make_graph.py
@@ -4,7 +4,7 @@ from numpy import genfromtxt
 import pandas as pd
 
 #my_data = genfromtxt('little_data.csv', delimiter=',')
-column_names = ['games', 'loss', 'q_mean','reward']
+column_names = ['iteration', 'step', 'perc','loss','q']
 df = pd.read_csv('test1.csv', sep=',', header=None,names=column_names)
 my_data = df.values
 
@@ -13,10 +13,11 @@ print (rows)
 its = []
 for i in range(0,rows):
 	its.append(i*100)
+fig = plt.figure()
 
-plt.scatter(its, df['reward'])
+plt.scatter(df['step'], df['q'])
 plt.show()
-plt.savefig('reward-iterations.pdf')
+fig.savefig('q.png')
 
 #make script
 #check out jivko's code
